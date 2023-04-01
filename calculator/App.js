f7aca2e63bf534f618936218f2ee8715084efc39
unknown <jmjtorrecampo@donbosco.edu.ph>
2023-04-01 16:51:14 +0800

    feat: Added new Files

---
+++ calculator/App.js
@@ -0,0 +1,40 @@
+
+import React, { Component } from 'react'
+import { View, Text, StyleSheet, Button, Alert } from 'react-native'
+
+class App extends Component{
+
+showAlert = () => {
+Alert.alert(
+'Message',
+'This is a message',
+[
+{
+text: 'OK',
+onPress: () => console.log('OK'),
+}
+]
+)
+}
+
+render(){
+return(
+<View style={styles.container}>
+<Text>Hey Beautiful!</Text>
+<Button title="Show Alert"
+onPress={this.showAlert}/>
+</View>
+)
+}
+}
+
+const styles = StyleSheet.create({
+container: {
+backgroundColor: "#40E0D0",
+flex: 1,
+alignItems: "center",
+justifyContent: "center",
+},
+})
+
+export default App;
